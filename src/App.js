ca31a37bdc1dee9860547903b2ac347a901885f0
Alexey Rodionov <alexey.rodionov.dev@gmail.com>
2023-07-07 15:09:39 +0600

    Implement contribution fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import useContribution from './hooks/useContribution';
+
+
+
 export default function App() {
+  const { list, loading }= useContribution();
+
   return (
     <div className='app-container'>
+      {loading && <p>Loading...</p>}
+
       Hello World!
+
+      {list.map(({date, contributionCount}) => (
+        <div key={date}>
+          <span>{date} - </span>
+          <span>{contributionCount}</span>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/hooks/useContribution.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { addDays, endOfWeek, format } from 'date-fns';
+
+import {getContributionList} from '../utils/api';
+
+const DAY_COUNT = 357;
+
+
+export default function useContribution() {
+  const [loading, setLoading] = useState(false);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        setLoading(true);
+      
+        const contributionList = await getContributionList();
+    
+        const today = new Date();
+        const endDate = endOfWeek(today, { weekStartsOn: 1 });
+        const startDate = addDays(endDate, -DAY_COUNT + 1);
+        const currentDate = new Date(startDate);
+    
+        const allDays = [];
+    
+        while(currentDate <= endDate) {
+          const formattedDate = format(new Date(currentDate), 'yyyy-MM-dd');
+          const contributionCount = contributionList[formattedDate] ?? 0;
+          allDays.push({date: formattedDate, contributionCount});
+    
+          currentDate.setDate(currentDate.getDate() + 1);
+        }
+        
+        setDays(allDays);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    load();
+  }, []);
+
+  return { list: days, loading };
+}
